f77df7679b8cbe0a07c7da50c9980f6f73fbba03
James Hughes <jmhghs7@gmail.com>
2016-07-01 15:14:07 -0700

    Assignment 2 Part 1

---
+++ assignment2_part1.js
@@ -0,0 +1,47 @@
+//Stings and String Manipulation
+
+//Step 1
+//var name=prompt("What is your name?");
+//alert(name.length);
+
+//Step 2
+//var name=prompt("What is your name?");
+//var n=prompt("Please enter a number corresponding to a letter in your name");
+//alert(name.charAt(n));
+
+//Step 3
+//var firstName=prompt("Please enter your first name.");
+//var lastName=prompt("Please enter your last name.");
+//alert("Your name is " + firstName.concat(lastName));
+
+//Step 4
+//var alphaPhrase="The quick brown fox jumped over the lazy dog.";
+//alert(alphaPhrase.indexOf("fox"));
+
+//Step 5
+//var alphaPhrase="The quick brown fox jumped over the lazy dog.";
+//alert(alphaPhrase.lastIndexOf("fox"));
+
+//Step 6
+//var alphaPhrase="The quick brown fox jumped over the lazy dog.";
+//var yourName=prompt("Please enter your full name");
+//alert(alphaPhrase.replace("the lazy dog", yourName));
+
+//Step 7
+//var alphaPhrase="The quick brown fox jumped over the lazy dog.";
+//var randomWord=prompt("Please enter any word.")
+//alert(randomWord.search(randomWord));
+
+//Step 8
+//var old_string="The quick brown fox jumped over the lazy dog.";
+//var new_string=old_string.slice(32, 44);
+//alert(new_string.toUpperCase());
+
+//Step 9
+//var allCaps="     THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.     ";
+//alert(allCaps.trim().toLowerCase());
+
+//Step 10
+//var lowerCase="the quick brown fox jumped over the lazy dog.";
+//alert(lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1, 45));
+
